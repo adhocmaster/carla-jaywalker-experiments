604e89cf2b8eb4d8a14182246448b5acd8f2a0a4
adhocmaster <adhocmaster@live.com>
2022-04-13 17:36:46 -0700

    another fix

--- research-runner.py
+++ research-runner.py
@@ -5,6 +5,8 @@ import os
 from lib import LoggerFactory, Utils
 from research import Research1v1
 
+from research import ResearchFactory
+
 
 @click.group()
 def chooseResearch():
@@ -40,17 +42,28 @@ def chooseResearch():
     help='Output directory (default logs)', 
     prompt=True
     )
-def r1v1(host, port, log, output_dir):
+@click.option(
+    '--max_ticks',
+    metavar='number',
+    default=100,
+    type=int,
+    help='Number of ticks the simulator will run'
+    )
+def r1v1(host, port, log, output_dir, max_ticks):
     # sample command: python research-runner.py r1v1 -h 127.0.0.1 -p 2000 --log INFO --output_dir logs
     # sample command: python research-runner.py r1v1 --host 127.0.0.1 --port 2000 --log INFO --output_dir logs
     click.echo(f"research chosen : R1v1 with host: {host}, port: {port}, log level: {log}, output directory: {output_dir}")
     
-    name = "Research1v1"
-    logPath = os.path.join(output_dir, f"{name}.log")
-    logger = LoggerFactory.getBaseLogger(name, defaultLevel=log, file=logPath)
-    client = Utils.createClient(logger, host, port)
-    research = Research1v1(client, log, output_dir)
-    research.run(maxTicks=100)
+    # name = "Research1v1"
+    # logPath = os.path.join(output_dir, f"{name}.log")
+    # logger = LoggerFactory.getBaseLogger(name, defaultLevel=log, file=logPath)
+    # client = Utils.createClient(logger, host, port)
+    # research = Research1v1(client, log, output_dir)
+    # research.run(maxTicks=100)
+
+    
+    research = ResearchFactory.createResearch1v1(host=host, port=port, defaultLogLevel=log, output_dir=output_dir)
+    research.run(maxTicks=max_ticks)
 
 
 
