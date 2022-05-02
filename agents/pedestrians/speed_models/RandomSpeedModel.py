c744a2769406fd902a72353e462d445a92ebdc1d
Bryan Rodriguez <bryanrodriguez@Bryans-MacBook-Pro.local>
2022-05-01 18:02:55 -0700

    added

--- agents/pedestrians/speed_models/RandomSpeedModel.py
+++ agents/pedestrians/speed_models/RandomSpeedModel.py
@@ -4,7 +4,7 @@ import random
 class RandomSpeedModel(SpeedModel):
     
     def initialize(self):
-        self._desiredSpeed = self.internalFactors["desired_speed"] * random.randrange(.1,.5, .1)
-        self._minSpeed = self.internalFactors["min_crossing_speed"] * random.randrange(1,2,.1)
-        self._maxSpeed = self.internalFactors["max_crossing_speed"] * random.randrange(1,2,.1)
+        self._desiredSpeed = self.internalFactors["desired_speed"] * random.uniform(.1,.5)
+        self._minSpeed = self.internalFactors["min_crossing_speed"] * random.uniform(1,2)
+        self._maxSpeed = self.internalFactors["max_crossing_speed"] * random.uniform(1,2)
         pass
