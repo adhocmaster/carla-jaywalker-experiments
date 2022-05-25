66556157ac132e469691b32670d6d4b87badadd9
jackson <javtran@ucsc.edu>
2022-05-25 16:11:57 -0700

    working on freeze and unfreeze

--- agents/pedestrians/factors/FreezingModel.py
+++ agents/pedestrians/factors/FreezingModel.py
@@ -4,11 +4,23 @@ import carla
 import numpy as np
 from ..StateTransitionModel import StateTransitionModel
 from ..PedState import PedState
+import random
 
 
 class FreezingModel(StateTransitionModel):
     # create an init to declare counter variable 
 
+     def __init__(self, agent: PedestrianAgent, actorManager: ActorManager, obstacleManager: ObstacleManager, internalFactors: InternalFactors, final_destination=None) -> None:
+
+        super().__init__(agent, actorManager, obstacleManager, internalFactors=internalFactors)
+
+        # self.haveSafeDestination = False # This is a sequential model. There are some stuff to do at start, and some stuff to do at end.
+        # self._destination = None
+        self.counter = 0
+        self.unfreezePeriod = 0
+        #TG = self.agent.getAvailableTimeGapWithClosestVehicle()
+        pass
+    
     @property
     def name(self):
         return f"FreezingModel #{self.agent.id}"
@@ -21,21 +33,25 @@ class FreezingModel(StateTransitionModel):
 
     
     def calculateForce(self):
+        while (True):
+            self.counter += 1000
         #TODO
-
         return None
 
     
     def canfreeze(self):
         #TODO
         conflictPoint = self.agent.getPredictedConflictPoint()
-        # figure outwhere get predictedcofnlictp oint and potentially modify 
         if conflictPoint is None:
             return None
         return True
 
 
     def canUnfreeze(self):
+        TTX = PedUtils.timeToCrossNearestLane(self.map, self.agent.location, self.agent._localPlanner.getDestinationModel().getDesiredSpeed())
+        self.agent.logger.info(f"TG:  {TG} and TTX: {TTX}")
+        self.counter -= 1000
+        self.unfreezePeriod = random.randint()
         #TODO
         # decrease counter 
         # decide if we want to range from 0 - tcc or tg or just tcc or all at once since we want random
