7b73e56a43e41ad70f0c0d8c753259501e917af0
Adhocmaster <adhocmaster@live.com>
2022-05-31 10:48:23 -0700

    working on metrics

--- lib/Geometry.py
+++ lib/Geometry.py
@@ -49,3 +49,9 @@ class Geometry:
         return clone
 
     
+    @staticmethod
+    def distanceTuples(point1, point2):
+        return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+
+    
