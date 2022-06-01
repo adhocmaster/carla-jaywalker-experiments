bb26ed1f4668bc788731bfc3665aeb51b79d5d05
Str4wbeary <xcai30@ucsc.edu>
2022-05-31 20:09:36 -0700

    Turns off if the vehicle is at low speed

--- agents/pedestrians/factors/AggressiveCrossingFactorModel.py
+++ agents/pedestrians/factors/AggressiveCrossingFactorModel.py
@@ -33,17 +33,17 @@ class AggressiveCrossingFactorModel(CrossingFactorModel, StateTransitionModel):
         d_x = (veh_loc.x - ped_loc.x) / veh_loc.distance(ped_loc)
         d_y = (veh_loc.y - ped_loc.y) / veh_loc.distance(ped_loc)
         d_z = (veh_loc.z - ped_loc.z) / veh_loc.distance(ped_loc)
-        d =carla.Vector3D(d_x, d_y, d_z)
-        # move toward original destination after passing conflict point
-        # nearest_distance = self.actorManager.distanceFromNearestOncomingVehicle()
-        # if nearest_distance < 5:
-        #     force = self.destDirection * magnitude
-        # else:
-        #     force = d * magnitude
+        d = carla.Vector3D(d_x, d_y, d_z)
+        force = d * magnitude
+        nearest_distance = self.actorManager.distanceFromNearestOncomingVehicle()
+        v_vel = veh.get_velocity()
+        if v_vel.x < 5 and v_vel.y < 5 and v_vel.z < 5:
+            force = self.destDirection * magnitude
+            return force
         
 
         # force = d * magnitude ###
-        force = self.destDirection * magnitude
+        # force = self.destDirection * magnitude
         return force
             
 
@@ -55,4 +55,34 @@ class AggressiveCrossingFactorModel(CrossingFactorModel, StateTransitionModel):
         return None # in other states this model does not produce force
     
     def getNewState(self):
+
+        # return None
+        if self.agent.isCrossing() == False:
+            return None
+
         return None
+
+        # self.agent.logger.info(f"Collecting state from {self.name}")
+
+        # # change to survival state if there is a near collision
+        # # get the collision point from the head of the vehicle and the center of the pedestrian 
+
+        # # if vehicle is too far, we don't need to even consider it
+        # TTC = self.actorManager.pedPredictedTTCNearestOncomingVehicle()
+        # if TTC is None:
+        #     return None
+
+        
+        # conflictPoint = self.agent.getPredictedConflictPoint()
+        # if conflictPoint is None:
+        #     return None
+
+        # TG = self.agent.getAvailableTimeGapWithClosestVehicle()
+        # TTX = PedUtils.timeToCrossNearestLane(self.map, self.agent.location, self.agent._localPlanner.getDestinationModel().getDesiredSpeed())
+        
+        # diff = TG - TTX # may be too far
+        # if diff < self.internalFactors["threshold_ttc_survival_state"] and diff > 0:
+        #     return PedState.SURVIVAL
+
+        # if self.internalFactors["threshold_ttc_survival_state"] > TG:
+        #     return PedState.SURVIVAL
