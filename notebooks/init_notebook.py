9f4cb3e74c61f2b21807c2eddd4044d756bcfc88
jackson <javtran@ucsc.edu>
2022-06-03 17:25:32 -0700

    run notebooks + add graphs

--- notebooks/init_notebook.py
+++ notebooks/init_notebook.py
@@ -5,7 +5,7 @@ try:
 except ValueError: # Already removed
     pass
 
-projectFolder = 'C:/AV/Carla/CARLA_0.9.13/WindowsNoEditor/PythonAPI/experiments'
+projectFolder = 'C:\\Users\\carla\\carla-jaywalker-experiments'
 # projectFolder = 'E:\\AV\\Carla\\CARLA_0.9.13\\WindowsNoEditor\\PythonAPI\\experiments'
 sys.path.append(str(projectFolder))
 os.chdir(projectFolder)
