9e46c6778113594aa911726acdb674fa67bb29bc
Str4wbeary <xcai30@ucsc.edu>
2022-06-06 19:27:49 -0700

    Update notebooks and graphs.

--- agents/pedestrians/gap_models/GapUtils.py
+++ agents/pedestrians/gap_models/GapUtils.py
@@ -4,5 +4,8 @@ class GapUtils:
 
     @staticmethod
     def sigmoid(x):
+        print("SIGMOID(X)... \n\n\n")
+        print(x)
+        print("\n\n\n")
         sig = 1 / (1 + math.exp(-x))
         return sig
--- notebooks/init_notebook.py
+++ notebooks/init_notebook.py
@@ -6,7 +6,8 @@ except ValueError: # Already removed
     pass
 
 # projectFolder = 'C:/AV/Carla/CARLA_0.9.13/WindowsNoEditor/PythonAPI/experiments'
-projectFolder = 'E:\\AV\\Carla\\CARLA_0.9.13\\WindowsNoEditor\\PythonAPI\\experiments'
+# projectFolder = 'E:\\AV\\Carla\\CARLA_0.9.13\\WindowsNoEditor\\PythonAPI\\experiments'
+projectFolder = 'C:\\Users\\ryanc\\Documents\\GitHub\\CM194\\carla-jaywalker-experiments'
 sys.path.append(str(projectFolder))
 os.chdir(projectFolder)
 print( f"current working dir{os.getcwd()}")
