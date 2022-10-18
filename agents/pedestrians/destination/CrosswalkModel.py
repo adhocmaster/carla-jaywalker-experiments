e6bec707da4e9cf42a8521c92c924281feb3f95e
Str4wbeary <xcai30@ucsc.edu>
2022-10-17 21:39:54 -0700

    updated createPolygon

--- agents/pedestrians/destination/CrosswalkModel.py
+++ agents/pedestrians/destination/CrosswalkModel.py
@@ -15,12 +15,43 @@ class CrosswalkModel:
     
     def createPolygonIfNone(self):
         # TODO: we make a generic one which may be based on real world dataset. Also create a goal line
-        pass
+        if self.goalLine == None:
+            self.createGoalLine(4)
+        start_x, start_y = self.source.coords[0][0], self.source.coords[0][1]
+        goalLine_x1, goalLine_y1 = self.goalLine.coords[0][0], self.goalLine.coords[0][1] 
+        goalLine_x2, goalLine_y2 = self.goalLine.coords[1][0], self.goalLine.coords[1][1]
+        # Bottom left point
+        botLeft_x = start_x - 0.5
+        botLeft_y = start_y
+        botLeft = shapely.Point((botLeft_x, botLeft_y))
+        # Bottom right point
+        botRight_x = start_x + 0.5
+        botRight_y = start_y
+        botRight = shapely.Point((botRight_x, botRight_y))
+        # Top left point
+        topLeft_x = goalLine_x1
+        topLeft_y = goalLine_y1
+        topLeft = shapely.Point((topLeft_x, topLeft_y))
+        # Top right point
+        topRight_x = goalLine_x2
+        topRight_y = goalLine_y2
+        topRight = shapely.Point((topRight_x, topRight_y))
+        # Mid left point
+        midLeft_x = min(botLeft_x, topLeft_x) + (max(botLeft_x, topLeft_x) - min(botLeft_x, topLeft_x))/1.5
+        midLeft_y = (topLeft_y - botLeft_y)/2
+        midLeft = shapely.Point((midLeft_x, midLeft_y))
+        # Mid right point
+        midRight_x = min(botRight_x, topRight_x) + (max(botRight_x, topRight_x) - min(botRight_x, topRight_x))/3
+        midRight_y = (topRight_y - botRight_y) / 2
+        midRight = shapely.Point((midRight_x, midRight_y))
+        # Build areaPolygon
+        areaPolygon = shapely.Polygon([botLeft, midLeft, topLeft, topRight, midRight, botRight])
+        self.areaPolygon = areaPolygon
 
     def createGoalLine(self, length):
         goal = self.idealDestination
         goalLine = shapely.LineString([goal.coords[0]-length, goal.coords[0]+length])
-        return goalLine
+        self.goalLine = goalLine
 
 
     def generateIntermediatePoints(self):
