5218de3b4b370084232ebd872421c3d17ec2aac6
Ryan Cai <xcai30@ucsc.edu>
2022-10-20 15:27:20 -0700

    added some functions for CrosswalkModel

--- agents/pedestrians/destination/CrosswalkModel.py
+++ agents/pedestrians/destination/CrosswalkModel.py
@@ -1,5 +1,6 @@
 import carla
 import shapely
+import math
 
 class CrosswalkModel:
     def __init__(self, source, idealDestination, areaPolygon=None, goalLine=None):
@@ -38,20 +39,15 @@ class CrosswalkModel:
         topRight = shapely.Point((topRight_x, topRight_y))
         # Mid left point
         midLeft_x = min(botLeft_x, topLeft_x) + (max(botLeft_x, topLeft_x) - min(botLeft_x, topLeft_x))/1.5
-        midLeft_y = (topLeft_y - botLeft_y)/2
+        midLeft_y = ((topLeft_y + topRight_y)/2 - botLeft_y)/2
         midLeft = shapely.Point((midLeft_x, midLeft_y))
         # Mid right point
         midRight_x = min(botRight_x, topRight_x) + (max(botRight_x, topRight_x) - min(botRight_x, topRight_x))/3
-        midRight_y = (topRight_y - botRight_y) / 2
+        midRight_y = ((topLeft_y + topRight_y)/2 - botRight_y)/2
         midRight = shapely.Point((midRight_x, midRight_y))
         # Build areaPolygon
         areaPolygon = shapely.Polygon([botLeft, midLeft, topLeft, topRight, midRight, botRight])
         self.areaPolygon = areaPolygon
-
-    def createGoalLine(self, length):
-        goal = self.idealDestination
-        goalLine = shapely.LineString([goal.coords[0]-length, goal.coords[0]+length])
-        self.goalLine = goalLine
 
 
     def generateIntermediatePoints(self):
@@ -82,3 +78,56 @@ class CrosswalkModel:
     def hasReachedNextDestinationPoint(self, agentLocation: carla.Location):
         # TODO: fill it out
         return False
+
+    def createGoalLine(self, length):
+        goal = self.idealDestination
+        #goalLine = shapely.LineString([goal.coords[0]-length, goal.coords[0]+length])
+        #self.goalLine = goalLine
+        pass
+
+
+# ------------------------------------------------------------------------------------------------------------
+    def degreeFromX(line):
+        # Find the angle in radian between two given lines
+        # Formula:
+        # atan2(vec2.y-vec1.y, vec2.x-vec1.x)
+        
+        # Extract points from line1 and line2
+        x = line.coords[1][0] - line.coords[0][0]
+        y = line.coords[1][1] - line.coords[0][1]
+
+        # Find theta using formula
+        theta = math.atan2(y, x)
+
+        return theta
+
+    def closestEnd(point, goalLine):
+        # Given a point A and a line Y, find the point B on Y such that the line AB is the shortest
+        d = goalLine.project(point)
+        end = goalLine.interpolate(d)
+        return end
+
+    def pointBetween(start, end, d):
+        # Given start point A and end point B, find a point C in between AB that is d distance away from A
+        line = shapely.LineString([start, end])
+        point = line.interpolate(d, normalized=True)
+        return point
+
+    def pointRotate(point, origin, degree=90):
+        # Given a point A and an origin O, rotate A +-90 degrees around O.
+        rotated_point = shapely.affinity.rotate(point, degree, origin=origin)
+        return rotated_point
+
+
+    def pointsOnLine(start, end, n):
+        # Given a start point A and an end point B, generate n number of evenly placed points between the line AB
+        pointsOL = [start]
+        line = shapely.LineString([start, end])
+        gap = 1 / (n + 1)
+        d = gap
+        for i in range(n):
+            point = line.interpolate(d, normalized=True)
+            d = d + gap
+            pointsOL.append(point)
+        pointsOL.append(end)
+        return pointsOL
