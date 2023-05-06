dfc3f3c7f90d30d849695399d688c4255498dd11
jawadefaj <abjawad@ucsc.edu>
2023-05-05 22:02:57 -0700

    update

--- highd_tools/highD/HighDStats.py
+++ highd_tools/highD/HighDStats.py
@@ -1,127 +1,98 @@
-
+import numpy as np
 import pandas as pd
 from .PlotHelper import Histogram
 from .ManeuverFilter import *
 
 
-# this class provides statistics for a single highD dataset 
-# the location of vehicle (x, y) are the top left corner of the vehicle !!!Important!!!
-
 class HighDStats():
     def __init__(self, DataContainer):
-
         self.data_container = DataContainer
-        # print("Inside HighD again stats")       
-        pass
-    
-    # we plot the velocity of both the vehicle and the preceding vehicle
+
     def plot_agent_following_scenario(self, follow_meta):
+        # Get the tracks dataframe from the data container
         tracks = self.data_container.tracks
 
+        # Extract information about the scenario from the follow_meta dataframe
         ego_id = follow_meta['ego_id']
         preceding_id = follow_meta['preceding_id']
         start_frame = follow_meta['start_frame']
         end_frame = follow_meta['end_frame']
 
+        # Create a title for the plot based on the scenario information
         title = f"Scenario {ego_id} - {preceding_id} - {end_frame - start_frame}"
 
+        # Initialize a dictionary to hold the data to plot
         plot_dict = {
             'frame': [],
             'ego_speed': [],
             'preceding_speed': [],
             'relative_speed': [],
             'relative_distance': [],
-            'ttc_ego':[],
-            'thw_ego':[],
-            'dhw_ego':[],
+            'ttc_ego': [],
+            'thw_ego': [],
+            'dhw_ego': [],
         }
 
+        # Loop through each frame in the scenario and extract relevant data
         for frame in range(start_frame, end_frame):
-            ego_speed = abs(tracks[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]['xVelocity'].values[0])
-            preceding_speed = abs(tracks[(tracks['id'] == preceding_id) & (tracks['frame'] == frame)]['xVelocity'].values[0])
+            ego_track = tracks.loc[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]
+            preceding_track = tracks.loc[(tracks['id'] == preceding_id) & (tracks['frame'] == frame)]
+
+            ego_speed = abs(ego_track['xVelocity'].values[0])
+            preceding_speed = abs(preceding_track['xVelocity'].values[0])
             relative_speed = ego_speed - preceding_speed
 
-            ego_x = tracks[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]['x'].values[0]
-            ego_y = tracks[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]['y'].values[0]
-            preceding_x = tracks[(tracks['id'] == preceding_id) & (tracks['frame'] == frame)]['x'].values[0]
-            preceding_y = tracks[(tracks['id'] == preceding_id) & (tracks['frame'] == frame)]['y'].values[0]
+            ego_x, ego_y = ego_track['x'].values[0], ego_track['y'].values[0]
+            preceding_x, preceding_y = preceding_track['x'].values[0], preceding_track['y'].values[0]
             relative_distance = np.sqrt((ego_x - preceding_x)**2 + (ego_y - preceding_y)**2)
 
-            ttc_ego = tracks[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]['ttc'].values[0]
-            thw_ego = tracks[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]['thw'].values[0]
-            dhw_ego = tracks[(tracks['id'] == ego_id) & (tracks['frame'] == frame)]['dhw'].values[0]
+            ttc_ego, thw_ego, dhw_ego = ego_track[['ttc', 'thw', 'dhw']].values[0]
 
-            # print('thw ', thw_ego)
-
-            # # clip ttc to 0 - 200
-            # ttc_ego = np.clip(ttc_ego, 0, 200)
-            # # clip thw to 0 - 10
-            # thw_ego = np.clip(thw_ego, 0, 10)
-            # # clip dhw to 0 - 300
-            # dhw_ego = np.clip(dhw_ego, 0, 300)
-
-            # print('relative ditance ', relative_distance)
-
+            # Add the extracted data to the plot_dict
             plot_dict['frame'].append(frame)
             plot_dict['ego_speed'].append(ego_speed)
             plot_dict['preceding_speed'].append(preceding_speed)
             plot_dict['relative_speed'].append(relative_speed)
             plot_dict['relative_distance'].append(relative_distance)
-            plot_dict['ttc_ego'].append(1/ttc_ego)
+            plot_dict['ttc_ego'].append(1 / ttc_ego)
             plot_dict['thw_ego'].append(thw_ego)
             plot_dict['dhw_ego'].append(dhw_ego)
 
+        # Convert the plot_dict to a dataframe for plotting
         plot_df = pd.DataFrame(plot_dict)
-        # plot_df['ego_speed'] = (plot_df['ego_speed'] - plot_df['ego_speed'].min()) / (plot_df['ego_speed'].max() - plot_df['ego_speed'].min())
-        # plot_df['preceding_speed'] = (plot_df['preceding_speed'] - plot_df['preceding_speed'].min()) / (plot_df['preceding_speed'].max() - plot_df['preceding_speed'].min())
-        # plot_df['relative_speed'] = (plot_df['relative_speed'] - plot_df['relative_speed'].min()) / (plot_df['relative_speed'].max() - plot_df['relative_speed'].min())
-        # # plot_df['relative_distance'] = (plot_df['relative_distance'] - plot_df['relative_distance'].min()) / (plot_df['relative_distance'].max() - plot_df['relative_distance'].min())
-        # plot_df['ttc_ego'] = (plot_df['ttc_ego'] - plot_df['ttc_ego'].min()) / (plot_df['ttc_ego'].max() - plot_df['ttc_ego'].min())
-        # plot_df['thw_ego'] = (plot_df['thw_ego'] - plot_df['thw_ego'].min()) / (plot_df['thw_ego'].max() - plot_df['thw_ego'].min())
-        # plot_df['dhw_ego'] = (plot_df['dhw_ego'] - plot_df['dhw_ego'].min()) / (plot_df['dhw_ego'].max() - plot_df['dhw_ego'].min())
-        # relative distance = dhw_ego when we scale with min and max
-        # relative speed = ttc ego vehicle when we scale with min and max
-    
-        plot_df.plot(x='frame', y=['ego_speed',  'preceding_speed'], title=title)
+
+        # Create a subplot for each type of data to plot, and set the titles
+        plot_df.plot(x='frame', y=['ego_speed', 'preceding_speed'], title=title)
         plot_df.plot(x='frame', y=['relative_speed'], title=title)
         plot_df.plot(x='frame', y=['relative_distance', 'dhw_ego'], title=title)
         plot_df.plot(x='frame', y=['ttc_ego', 'thw_ego'], title=title)
-        pass
 
-    
 
     def nFrames_vehicle_follow(self, follow_meta):
         df = follow_meta.copy(deep=True)
         df['nFrames'] = df['end_frame'] - df['start_frame']
         return np.sum(df['nFrames'].values)
 
+
     def follow_distance_distribution(self, follow_meta):
         tracks = self.data_container.tracks
         distance = []
-        # print("follow meta ", follow_meta.columns, len(follow_meta))
-        # print("tracks ", tracks.columns, len(tracks))
-        for index, row in follow_meta.iterrows():
-            ego_id = row['ego_id']
-            preceding_id = row['preceding_id']
-            start_frame = row['start_frame']
-            end_frame = row['end_frame']
-
-            ego_track = tracks[tracks['id'] == ego_id]
-            ego_track = ego_track[ego_track['frame'].between(start_frame, end_frame)]
-            ego_x, ego_y = ego_track['x'].values, ego_track['y'].values
+        
+        for _, row in follow_meta.iterrows():
+            ego_id, preceding_id, start_frame, end_frame = row[['ego_id', 'preceding_id', 'start_frame', 'end_frame']]
             
-            preceding_track = tracks[tracks['id'] == preceding_id]
-            preceding_track = preceding_track[preceding_track['frame'].between(start_frame, end_frame)]
-            preceding_x, preceding_y = preceding_track['x'].values, preceding_track['y'].values
-
+            ego_track = tracks[(tracks['id'] == ego_id) & (tracks['frame'].between(start_frame, end_frame))]
+            ego_x, ego_y = ego_track[['x', 'y']].values.T
+            
+            preceding_track = tracks[(tracks['id'] == preceding_id) & (tracks['frame'].between(start_frame, end_frame))]
+            preceding_x, preceding_y = preceding_track[['x', 'y']].values.T
+            
             temp_distance = np.sqrt((ego_x - preceding_x)**2 + (ego_y - preceding_y)**2)
             distance.extend(temp_distance)
 
-        df = pd.DataFrame(distance, columns=['followDistance'])
-        # df = df[df['followDistance'] < 50]
-        print("df ", df.columns, len(df))
+        df = pd.DataFrame({'followDistance': distance})
         Histogram.plotMetricsDF(df, 'followDistance', xlabel='Follow Distance (m)', bins=10, kde=True)
-        pass
+
 
     def follow_relative_speed_distribution(self, follow_meta):
         tracks = self.data_container.tracks
@@ -146,29 +117,25 @@ class HighDStats():
         df = pd.DataFrame(relative_speed, columns=['RelativeSpeed'])
         Histogram.plotMetricsDF(df, 'RelativeSpeed', xlabel='Relative Speed', bins=10, kde=True)
 
+    import numpy as np
+
     def follow_speed_distance_ratio(self, follow_meta):
-
+        # Get the tracks dataframe from the data container
         tracks = self.data_container.tracks
-        distance = []
-        relative_speed = []
-        ratio = []
 
-        for index, row in follow_meta.iterrows():
-            ego_id = row['ego_id']
-            preceding_id = row['preceding_id']
-            start_frame = row['start_frame']
-            end_frame = row['end_frame']
+        # Initialize lists to hold the distance, relative speed, and ratio data
+        distance, relative_speed, ratio = [], [], []
 
-            ego_track = tracks[tracks['id'] == ego_id]
-            ego_track = ego_track[ego_track['frame'].between(start_frame, end_frame)]
+        # Loop through each follow scenario and extract relevant data
+        for _, row in follow_meta.iterrows():
+            ego_id, preceding_id, start_frame, end_frame = row[['ego_id', 'preceding_id', 'start_frame', 'end_frame']]
 
-            ego_x, ego_y = ego_track['x'].values, ego_track['y'].values
+            ego_track = tracks[(tracks['id'] == ego_id) & (tracks['frame'].between(start_frame, end_frame))]
+            ego_x, ego_y = ego_track[['x', 'y']].values.T
             ego_speed = ego_track['xVelocity'].values
             
-            preceding_track = tracks[tracks['id'] == preceding_id]
-            preceding_track = preceding_track[preceding_track['frame'].between(start_frame, end_frame)]
-
-            preceding_x, preceding_y = preceding_track['x'].values, preceding_track['y'].values
+            preceding_track = tracks[(tracks['id'] == preceding_id) & (tracks['frame'].between(start_frame, end_frame))]
+            preceding_x, preceding_y = preceding_track[['x', 'y']].values.T
             preceding_speed = preceding_track['xVelocity'].values
 
             temp_distance = np.sqrt((ego_x - preceding_x)**2 + (ego_y - preceding_y)**2)
@@ -178,17 +145,13 @@ class HighDStats():
             relative_speed.extend(del_speed)
 
             temp_ratio = abs(del_speed) / temp_distance
-
             ratio.extend(temp_ratio)
-            # print('index = ', index)
-            # break
-        # df = pd.DataFrame(ratio, columns=['SpeedDistanceRatio'])
+
+        # Create a dataframe from the extracted data
         df = pd.DataFrame({'distance': distance, 'relative_speed': relative_speed, 'ratio': ratio})
 
+        # Plot the distance vs relative speed data
         Histogram.plotXY(df, 'distance', 'relative_speed', xlabel='Distance (m)', ylabel='Relative Speed (m/s)')
-        pass
-    
-
 
 
     def min_velocity_distribution(self):
@@ -225,16 +188,5 @@ class HighDStats():
         tracksMeta = tracksMeta[tracksMeta['minTTC'] > 0]
         tracksMeta = tracksMeta[tracksMeta['minTTC'] < 200]
         Histogram.plotMetricsDF(tracksMeta, 'minTTC', xlabel='Min TTC (s)', bins=100, kde=True)
-        pass
-
-    def min_acceleration(self):
-        tracksMeta = self.data_container.tracksMeta.copy()
-        tracks = self.data_container.tracks.copy()
-
-        # group tracks by id
-        grouped_tracks = tracks.groupby('id')
-        
-
-        Histogram.plotMetricsDF(tracksMeta, 'minXAcceleration', xlabel='Min Acceleration (m/s^2)', bins=100, kde=True)
         pass
  
--- highd_tools/highD/PlotHelper.py
+++ highd_tools/highD/PlotHelper.py
@@ -8,20 +8,11 @@ class Histogram:
 
     @staticmethod
     def plotXY(data, x, y, xlabel="", ylabel=""):
-        X = data[x].values
-        Y = data[y].values
-        #  create a figure and axis
         fig, ax = plt.subplots()
-        #  plot the data
-        ax.scatter(X, Y)
-        #  set the title
-        ax.set_title(f'Scatter Plot {xlabel} vs {ylabel}')
-        #  set the x and y labels
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel(ylabel)
-        #  show the plot
+        ax.scatter(data[x].values, data[y].values)
+        ax.set(title=f'Scatter Plot {xlabel} vs {ylabel}', xlabel=xlabel, ylabel=ylabel)
         plt.show()
-    
+
     @staticmethod
     def plotNormalizedMetricsDF(data, col, name, bins=10):
         g = sns.displot(data=data, x=col, kde=True, stat="probability", bins=bins)
@@ -32,37 +23,26 @@ class Histogram:
     @staticmethod
     def plotMetricsDF(data, col, xlabel="", bins=10, kde=True):
         g = sns.displot(data=data, x=col, kde=kde, bins=bins)
-        if xlabel != "":
-            g.set_axis_labels(xlabel, "Count")
-
-        g.set_titles(f"Distribution of {xlabel}")
+        g.set_axis_labels(xlabel if xlabel else col, "Count")
+        g.set_titles(f"Distribution of {xlabel if xlabel else col}")
         plt.axvline(x=data[col].mean(), color='red')
         plt.show()
 
-    
     @staticmethod
     def plot2MetricsDF(data, col1, col2, bins=10, title="", xlabel=""):
         g = sns.displot(data=data, x=col1, hue=col2, kde=True, stat="probability", bins=bins, palette="colorblind")
-        # g.set_axis_labels(name, "Number of Intersections")
-        g.set_titles(f"{title}")
+        g.set_titles(title if title else f"Distribution of {col1} by {col2}")
         plt.show()
 
     @staticmethod
     def plot2StackedMetricsDF(data, col1, col2, bins=10, title="", xlabel=""):
         g = sns.displot(data=data, x=col1, hue=col2, multiple="stack", stat="probability", bins=bins, palette="colorblind")
-        # g.set_axis_labels(name, "Number of Intersections")
-        g.set_titles(f"{title}")
+        g.set_titles(title if title else f"Distribution of {col1} by {col2}")
         plt.show()
 
     @staticmethod
     def plot2MetricsDFSep(data, col1, col2, bins=10, title="", xlabel=""):
         g = sns.displot(data=data, x=col1, col=col2, kde=True, stat="probability", bins=bins, palette="colorblind")
-
-        if xlabel != "":
-            g.set_axis_labels(xlabel, "probability")
-
-        if title != "":
-            g.set_titles(title)
+        g.set_axis_labels(xlabel if xlabel else col1, "probability")
+        g.set_titles(title if title else f"Distribution of {col1} by {col2}")
         plt.show()
-
-
