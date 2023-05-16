7e541de89d1b40a6159bba003d54b04e963c048e
jawadefaj <jawadefaj006@gmail.com>
2023-05-15 18:41:24 -0700

    config file  update

--- highd_tools/config.py
+++ highd_tools/config.py
@@ -3,17 +3,13 @@
 
 # Directory of the project (path for the highD_tools folder)
 
-PROJECT_DIR = r'C:\\Users\\abjaw\\Documents\\GitHub\\CogMod-driver-behavior-model\\highd_tools'
-
-# Path for the HighD dataset  
-DATA_DIRECTORY = r'C:\\Users\\abjaw\\Documents\\GitHub\\highD-dataset\\Python\\data'
-
+PROJECT_DIR = r'C:\\Users\\abjawad\\Documents\\GitHub\\cogMod-driver-behavior-model\\highd_tools'
+# Path for the HighD dataset
+DATA_DIRECTORY = r'D:\\highD_data\\highD_dataset'
 # Path for the output directory
-OUTPUT_DIRECTORY = r"C:\\Users\\abjaw\\Documents\\GitHub\\CogMod-driver-behavior-model\\highd_tools\\output"
-
+OUTPUT_DIRECTORY = r"C:\\Users\\abjaw\\Documents\\GitHub\\cogMod-driver-behavior-model\\highd_tools\\output"
 # Path for the dill directory
-DILLDIR = r"C:\\Users\\abjaw\\Documents\\GitHub\\CogMod-driver-behavior-model\\highd_tools\\dill"
-
+DILLDIR = r"C:\\Users\\abjaw\\Documents\\GitHub\\cogMod-driver-behavior-model\\highd_tools\\dill"
 
 
 #  scale factor for drawing vehicles on the highway image 
