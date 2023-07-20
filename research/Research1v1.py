ff25935aa2a2336cc83c0dda196aff9625c0351a
adhocmaster <adhocmaster@live.com>
2023-07-19 23:23:49 -0700

    Update Research1v1.py

--- research/Research1v1.py
+++ research/Research1v1.py
@@ -60,6 +60,8 @@ class Research1v1(BaseResearch):
         self.maxStepsPerCrossing = maxStepsPerCrossing
         self.settingsId = settingsId
 
+        self.optionalFactors = []
+        # self.optionalFactors = [Factors.SURVIVAL_DESTINATION]
         # self.optionalFactors = [Factors.DRUNKEN_WALKER]
         # self.optionalFactors = [Factors.CROSSING_ON_COMING_VEHICLE]
         # self.optionalFactors = [Factors.CROSSING_ON_COMING_VEHICLE, Factors.SURVIVAL_DESTINATION]
@@ -67,7 +69,6 @@ class Research1v1(BaseResearch):
         # self.optionalFactors = [Factors.CROSSING_ON_COMING_VEHICLE, Factors.SURVIVAL_DESTINATION]
         # self.optionalFactors = [Factors.CROSSING_ON_COMING_VEHICLE, Factors.SURVIVAL_DESTINATION, Factors.DRUNKEN_WALKER]
         # self.optionalFactors = [Factors.CROSSING_ON_COMING_VEHICLE, Factors.DRUNKEN_WALKER]
-        self.optionalFactors = []
 
         # self.optionalFactors = [Factors.SURVIVAL_DESTINATION, Factors.DRUNKEN_WALKER, Factors.FREEZING_FACTOR]
 
