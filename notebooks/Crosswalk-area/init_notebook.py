c5d86d7728191c0f0dc1b805bddf583b3dbe8980
Adhocmaster <adhocmaster@live.com>
2024-01-30 14:41:26 -0800

    working on shape comparison

--- notebooks/Crosswalk-area/init_notebook.py
+++ notebooks/Crosswalk-area/init_notebook.py
@@ -6,8 +6,8 @@ except ValueError: # Already removed
     pass
 
 
-projectFolder = 'C:/AV/Carla/CARLA_0.9.13/WindowsNoEditor/PythonAPI/experiments'
-# projectFolder = 'E:\\AV\\Carla\\CARLA_0.9.13\\WindowsNoEditor\\PythonAPI\\experiments'
+# projectFolder = 'C:/AV/Carla/CARLA_0.9.13/WindowsNoEditor/PythonAPI/experiments'
+projectFolder = 'E:\\AV\\Carla\\CARLA_0.9.13\\WindowsNoEditor\\PythonAPI\\experiments'
 # projectFolder = 'C:\\Users\\ryanc\\Documents\\UCSC\\carla-jaywalker-experiments'
 sys.path.append(str(projectFolder))
 os.chdir(projectFolder)
